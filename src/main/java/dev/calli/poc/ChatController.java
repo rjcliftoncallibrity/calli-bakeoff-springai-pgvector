99c8456697ef87df481d1601a0214d91f44ef97e
Rachel Clifton <rachel@rachels-air.lan>
2025-02-17 11:49:10 -0500

    Updating chat client user text advise and system text

--- src/main/java/dev/calli/poc/ChatController.java
+++ src/main/java/dev/calli/poc/ChatController.java
@@ -26,19 +26,20 @@ public class ChatController {
                         You are an intelligent assistant for Callibrity.
                         A user will ask you questions, and it is your job to return a list of related files and content given the context and provided history information and not prior knowledge. If you cannot find any related files, then let the user know. Do not use general knowledge or hallucinate an answer.
                         Unless the user is asking for information from a specific source, be thorough and use all of your tools to search for related content. If the user is looking for content in general, search for matching content.
-                        To answer the user's query, make sure you cite the source, article title, author, and URL when mentioning their content. For other content, mention the file path or source from which it came.
+                        To answer the user's query, list the resources related to the request organized by category along with information about what makes the resource relevant to the request.
+                        Make sure you cite the source, article title, author, and URL when mentioning their content. For other content, mention the file path or source from which it came.
                         """)
                 .defaultAdvisors(
                         new MessageChatMemoryAdvisor(new InMemoryChatMemory()),
-                        new QuestionAnswerAdvisor(vectorStore, SearchRequest.builder().build(), """
-                        Context information is below, surrounded by ---------------------
+                        new QuestionAnswerAdvisor(vectorStore, SearchRequest.builder().topK(10).build(), """
+                        Relevant Documents are below, surrounded by ---------------------
                         
                         ---------------------
                         {question_answer_context}
                         ---------------------
                         
                         Unless the user is asking for information from a specific source, be thorough and use all of your tools to search for related content. If the user is looking for content in general, search for matching content.
-                        To answer the user's query, list the resources related to the request organized by category along with information about what makes the resource relevant to the request. 
+                        To answer the user's query, list the resources related to the request organized by category along with information about what makes the resource relevant to the request.
                         Make sure you cite the source, article title, author, and URL when mentioning their content. For other content, mention the file path or source from which it came.
                         """),
                         new SimpleLoggerAdvisor())
@@ -50,18 +51,19 @@ public class ChatController {
         Scanner input = new Scanner(System.in);
 
         System.out.println("\nHi! I'm Calli, your helpful Callibrity Chat Bot.");
-        System.out.println("\nWhat information are you looking for today? (Type 'Done' to end the chat)");
+        System.out.println("What information are you looking for today? (Type 'Done' to end the chat)");
 
         UUID chatId = UUID.randomUUID();
         String userText = input.nextLine();
         while(!"Done".equals(userText)) {
             System.out.println( chatClient.prompt()
                     .user(userText)
-                            .advisors(a -> a
-                                    .param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId)
-                                    .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 100))
+                    .advisors(a -> a
+                            .param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId)
+                            .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 100))
                     .call()
                     .content());
+            System.out.println("\n (Type 'Done' to end the chat)");
             userText = input.nextLine();
         }
         System.out.println("Thank you for chatting with Calli. Have a great day!");
